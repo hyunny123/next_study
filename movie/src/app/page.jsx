8026e14ba3055737c0595343fb2a29d7ff6f760c
hyunjung <hazelc3837@gmail.com>
2023-05-18 08:27:23 +0900

    FEAT:install tailwindcss

--- movie/src/app/page.jsx
+++ movie/src/app/page.jsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <>
+    <h1 className="text-blue-700">home</h1>
+    <h3>hi</h3>
+
+    </>
+  );
 }
