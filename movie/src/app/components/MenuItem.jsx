8fcd2a64274f4eddaaabb7fc2d9d4f5ee7db25e7
hyunjung <hazelc3837@gmail.com>
2023-05-20 22:02:47 +0900

    FEAT:header layout

--- movie/src/app/components/MenuItem.jsx
+++ movie/src/app/components/MenuItem.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import Link from "next/link";
 
-export default function MenuItem({title,address,Icon}) {
+export default function MenuItem({ title, address, Icon }) {
   return (
-    <div>MenuItem</div>
-  )
+    <div>
+      <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600">
+        <Icon classNam="text-2xl sm:hidden mx-4" />
+        <p className="hidden sm:inline my-2 text-sm">{title}</p>
+      </Link>
+    </div>
+  );
 }
