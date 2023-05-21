a0f996a2dccf77492e3b5cf77b116dc933ad0dea
hyunjung <hazelc3837@gmail.com>
2023-05-21 15:22:56 +0900

    FEAT: install useTheme & header 다크모드 변경

---
+++ movie/src/app/components/DarkModeSwitch.jsx
@@ -0,0 +1,24 @@
+"use client";
+import React from "react";
+import { MdLightMode, MdDarkMode } from "react-icons/md";
+import { useTheme } from "next-themes";
+
+export default function DarkModeSwitch() {
+  const { SystemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? SystemTheme : theme;
+  return (
+    <>
+      {currentTheme === "dark" ? (
+        <MdLightMode
+          className="text-xl cursor-pointer hover:text-amber-500"
+          onClick={() => setTheme("light")}
+        />
+      ) : (
+        <MdDarkMode
+          className="text-xl cursor-pointer hover:text-amber-500"
+          onClick={() => setTheme("dark")}
+        />
+      )}
+    </>
+  );
+}
--- movie/src/app/layout.jsx
+++ movie/src/app/layout.jsx
@@ -1,25 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Header from './components/Header'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Header from "./components/Header";
+import Providers from "./Providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Movie website',
-  description: 'This is the IMDb clone website',
-}
+  title: "Movie website",
+  description: "This is the IMDb clone website",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* header */}
-        <Header/>
-        {/* navbar */}
+        <Providers>
+          {/* header */}
+          <Header />
+          {/* navbar */}
 
-        {/* searchBox */}
-        
-        {children}</body>
+          {/* searchBox */}
+
+          {children}
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
