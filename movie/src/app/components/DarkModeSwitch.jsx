f725953732f2b216c631a753b478f17fbb777f14
hyunjung <hazelc3837@gmail.com>
2023-05-21 15:36:30 +0900

    FEAT: darkmode 마무리

--- movie/src/app/components/DarkModeSwitch.jsx
+++ movie/src/app/components/DarkModeSwitch.jsx
@@ -1,24 +1,29 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 
 export default function DarkModeSwitch() {
   const { SystemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+
   const currentTheme = theme === "system" ? SystemTheme : theme;
   return (
     <>
-      {currentTheme === "dark" ? (
-        <MdLightMode
-          className="text-xl cursor-pointer hover:text-amber-500"
-          onClick={() => setTheme("light")}
-        />
-      ) : (
-        <MdDarkMode
-          className="text-xl cursor-pointer hover:text-amber-500"
-          onClick={() => setTheme("dark")}
-        />
-      )}
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            className="text-xl cursor-pointer hover:text-amber-500"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <MdDarkMode
+            className="text-xl cursor-pointer hover:text-amber-500"
+            onClick={() => setTheme("dark")}
+          />
+        ))}
     </>
   );
 }
