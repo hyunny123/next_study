944549da6b7a45d4ccff1d9974ae0cd31ca2ea96
hyunjung <hazelc3837@gmail.com>
2023-05-23 00:04:47 +0900

    FEAT trending fetch data from tmdb api

--- movie/src/app/page.jsx
+++ movie/src/app/page.jsx
@@ -1,11 +1,47 @@
 import Image from "next/image";
+import Results from "./components/Results";
 
-export default function Home() {
+const API_KEY = process.env.API_KEY;
+const API_KEYS = process.env.API_KEYS;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fecthTrending";
+  // const res = await fetch(
+  //   `https://api.themoviedb.org/3/${
+  //     genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  //   }?api_key=${API_KEY}&language=en-US&page=1`,
+  //   { next: { revalidate: 10000 } }
+  // );
+  // const res = await fetch(`https://api.themoviedb.org/3/trending/movie/{time_window}`)
+  // const data = await res.json();
+  // const result = data.results;
+  // console.log(result);
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${API_KEYS}`,
+    },
+  };
+  const res = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?language=en-US",
+    // `https://api.themoviedb.org/3/trending/movie/${
+    //   genre === "fetchTopRated" ? "top_rated" : "day"
+    // }?language=en-US&page=1`,
+    options
+  );
+
+  if (!res.ok) {
+    throw new Error("fetch data error");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+
+
   return (
-    <>
-    <h1 className="text-blue-700">home</h1>
-    <h3>hi</h3>
-
-    </>
+    <div>
+      <Results results={results} />
+    </div>
   );
 }
