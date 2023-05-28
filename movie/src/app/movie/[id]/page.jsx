2c7499632c7bdee432bb8e8f7b167f59d4ea9d94
hyunjung <hazelc3837@gmail.com>
2023-05-29 01:43:32 +0900

    FEAT moviepage image 깨짐

--- movie/src/app/movie/[id]/page.jsx
+++ movie/src/app/movie/[id]/page.jsx
@@ -1,12 +1,35 @@
+import Image from "next/image";
 import React from "react";
 
-
-
-export default async function MoviePage(params) {
-  const movieId = params.movie_id;
+async function getMovie(movieId) {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.API_KEY}`
   );
-  const movie = await res.json();
-  return <div>page</div>;
+  return await res.json();
+}
+
+export default async function MoviePage(params) {
+  const movieId = params.id;
+  const movie = await getMovie(movieId);
+  return (
+    <div className="w-full">
+      <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            movie.backdrop_path || movie.poster_path
+          }`}
+          width={500}
+          height={300}
+          className="rounded-lg"
+          style={{
+            maxWidth: "100%",
+            height: "100%",
+          }}
+          placeholder="blur"
+          blurDataURL="../../../public/loader-1.svg"
+          alt="movie poster"
+        ></Image>
+      </div>
+    </div>
+  );
 }
