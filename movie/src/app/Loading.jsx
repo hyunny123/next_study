2341c34db9bb35405cf7c737b8fecdf563056544
hyunjung <hazelc3837@gmail.com>
2023-06-06 16:04:43 +0900

    FIX loading

--- movie/src/app/Loading.jsx
+++ movie/src/app/Loading.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import "./Loading.module.css";
 
 export default function Loading() {
   return (
     <div className="flex justify-center">
-      <img src="/public/90-ring.svg" alt="loader..." />
+      <h1 className="loading-h1">
+        LOADING
+        <span>.</span>
+        <span>.</span>
+        <span>.</span>
+      </h1>
     </div>
   );
 }
