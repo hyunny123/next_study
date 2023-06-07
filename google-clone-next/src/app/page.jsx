5d8914f91424dc793166515f8ad78b8019aea4f9
hyunjung <hazelc3837@gmail.com>
2023-06-08 07:57:52 +0900

    FEAT google image 가져오기 & next.config.js

--- google-clone-next/src/app/page.jsx
+++ google-clone-next/src/app/page.jsx
@@ -1,10 +1,21 @@
+import Image from "next/image";
 import React from "react";
 import HomeHeader from "./components/HomeHeader";
 
 export default function Home() {
   return (
     <>
-    <HomeHeader/>
+      {/* header */}
+      <HomeHeader />
+      {/* body */}
+      <div className="">
+        <Image
+          width="300"
+          height="100"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
+        />
+      </div>
+      {/* footer */}
     </>
-  )
+  );
 }
