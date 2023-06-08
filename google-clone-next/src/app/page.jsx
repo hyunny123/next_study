33f8b3e0e328fc9264f01736a85a04fce3f48470
hyunjung <hazelc3837@gmail.com>
2023-06-09 08:20:03 +0900

    FEAT searchform layout & global.css layer 사용

--- google-clone-next/src/app/page.jsx
+++ google-clone-next/src/app/page.jsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
+import { TbHomeSearch } from "react-icons/tb";
 import HomeHeader from "./components/HomeHeader";
+import HomeSearch from "./components/HomeSearch";
 
 export default function Home() {
   return (
@@ -8,13 +10,14 @@ export default function Home() {
       {/* header */}
       <HomeHeader />
       {/* body */}
-      <div className="">
+      <div className="flex flex-col items-center">
         <Image
           width="300"
           height="100"
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/800px-Google_2015_logo.svg.png"
         />
       </div>
+      <HomeSearch />
       {/* footer */}
     </>
   );
